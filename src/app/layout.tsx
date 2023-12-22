9656a23ffd2327ac6b7121ee13131323a1bc5137
drlsn <dariusz.lusnia@gmail.com>
2023-12-22 21:42:59 +0100

    Add new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Lora } from 'next/font/google'
+import localFont from '@next/font/local'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const font = Lora({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
--- src/components/TaskListItem.tsx
+++ src/components/TaskListItem.tsx
@@ -23,14 +23,14 @@ const bgs = {
 
 export default function TaskListItem(props: TaskListItemProps) {
   return (
-    <div className={`border-${props.color} hover:bg-gray-100 min-h-8 h-fit flex gap-4 w-full px-1 items-center border-b-0`}>
+    <div className={`hover:bg-gray-100 min-h-9 h-fit flex gap-4 w-full px-1 items-center border-gray-600 overflow-x-hidden`} style={{ borderBottom: "1px solid rgb(200, 200, 200)" }}>
       <div className="w-[105px] flex justify-between">
         <span className="font-bold w-[50px] text-nowrap">{props.startTime}</span>
         <span className="w-[5px]">-</span>
         <span className="font-bold w-[50px] text-nowrap text-end">{props.endTime}</span>
       </div>
       <span className="text-nowrap">{props.children}</span>
+      <span className="font-bold justify-self: end">+</span>
     </div>
   )
 }
-
